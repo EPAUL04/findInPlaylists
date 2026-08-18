f728c6eb12bb55f000a6bf2ff244339143fd67b8
Elizabeth Paul <eandepaul@gmail.com>
2026-08-18 15:13:17 -0700

    testing

--- js/script.js
+++ js/script.js
@@ -158,7 +158,7 @@ async function checkAllPlaylists(song) {
             headers: { Authorization: `Bearer ${token}` }
         });
         const result2 = await playlistRequest2.json();
-        result2.items.forEach(song => {
+        result2.items.forEach(async song => {
             alert("comparing " + song.name + " and " + songGlobal.name); 
             if (song.name == songGlobal.name) {
                 alert(playlist + " has song");
