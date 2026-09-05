3352c9ba14fc3fa54b6061a1df803a32da35588b
Elizabeth Paul <eandepaul@gmail.com>
2026-09-05 14:35:36 -0700

    testing song.track.TrackObject.name

--- js/script.js
+++ js/script.js
@@ -170,13 +170,13 @@ async function checkAllPlaylists(song) {
     // check for song in each
     result.items.forEach(async playlist => {
       if (playlist.owner.id == myID) {
-        alert("playlist " + playlist.name);
+        // alert("playlist " + playlist.name);
         const playlistRequest2 = await fetch(`https://api.spotify.com/v1/playlists/${playlist.id}`, {
             headers: { Authorization: `Bearer ${token}` }
         });
         const result2 = await playlistRequest2.json();
         result2.items.items.forEach(song => {
-            alert("comparing " + song.track.name + " and " + songGlobal.name); 
+            alert("comparing " + song.track.TrackObject.name + " and " + songGlobal.name); 
             if (song.track.name == songGlobal.name) {
                 alert(playlist + " has song");
                 playlistsGlobal += playlist + ", ";
